6aaaf0ab03d924b4ae81967d09d62426f04a189f
Alsciende <bertolini.cedric@me.com>
2017-05-14 17:23:33 +0200

    release 0.0.1

--- vue/src/services/configService.js
+++
@@ -1,9 +0,0 @@
-let api_base_url = 'http://ashesdb.local:8080/app_dev.php'
-let image_base_url = 'http://ashesdb.local:8080/bundles/card_images/'
-
-export default {
-  api_base_url,
-  getCardImageURL: function (card) {
-    return image_base_url + card.code + '.jpg'
-  }
-}
